762b27b9571b5ce85a6f7bea1034b069af6e4bdb
Andrew <gitdiwax@gmail.com>
2026-05-06 09:21:48 +0300

    add postDetails page

---
+++ PawPost/frontend/src/pages/PostDetailsPage.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { getPostById } from "../api/postsApi";
+import type { AnimalPost } from "../types/post";
+
+export function PostDetailsPage() {
+  const { id } = useParams();
+
+  const [post, setPost] = useState<AnimalPost | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    if (!id) return;
+
+    getPostById(id)
+      .then(setPost)
+      .catch(() => setError("Пост не найден"))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  if (loading) return <p>Загрузка...</p>;
+  if (error) return <p>{error}</p>;
+  if (!post) return <p>Пост не найден</p>;
+
+  return (
+    <main className="page">
+      <Link to="/" className="back-link">
+        ← Назад
+      </Link>
+
+      <article className="details">
+        {post.imageUrl && (
+          <img src={post.imageUrl} alt={post.title} className="details__image" />
+        )}
+
+        <span className="details__category">
+          {post.category || "Без категории"}
+        </span>
+
+        <h1>{post.title}</h1>
+        <p className="details__short">{post.shortDescription}</p>
+        <p className="details__full">{post.fullDescription}</p>
+      </article>
+    </main>
+  );
+}
